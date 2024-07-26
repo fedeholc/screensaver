87fb558435accf817b315a4539ad6572dd0856f9
Federico Holc <federicoholc@gmail.com>
2024-07-26 18:29:08 -0300

    feat: Update page background image handling

--- app/p-bg/page.tsx
+++ app/p-bg/page.tsx
@@ -1,16 +1,69 @@
 /* eslint-disable @next/next/no-img-element */
-
+"use client";
 import styles from "./page.module.css";
 import FullScreenButton from "../fullScreenButton";
-import img1 from "/a.jpg";
+import img1 from "../../public/c.jpg";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function Home() {
-  //let bg = { backgroundImage: { img1 } };
-  let bg = { backgroundImage: 'url("/a.jpg")' };
+  //let bg = { backgroundImage: `url(${img1.src})` };
+
+  const images = [
+    `url(${img1.src})`,
+    'url("/b.jpg")',
+    `url('https://content.magnumphotos.com/wp-content/uploads/2016/04/cortex/par115311-overlay.jpg')`,
+    // Agrega más URLs de imágenes según sea necesario
+  ];
+
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  let bg = {
+    backgroundImage: `url('https://content.magnumphotos.com/wp-content/uploads/2016/04/cortex/par115311-overlay.jpg')`,
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (!isPlaying) return;
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 2000);
+    return () => clearInterval(intervalId);
+  }, [images.length, isPlaying]);
+
+  const currentImage = images[currentImageIndex];
+
+  //también podría usar useref?
 
   return (
-    <main style={bg} className={styles.main}>
-      <FullScreenButton />
+    <main
+      style={{ backgroundImage: `${currentImage}` }}
+      className={styles.main}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+        }}
+      >
+        <div>
+          <FullScreenButton />
+          <button
+            onClick={() => {
+              if (isPlaying) {
+                setIsPlaying(false);
+              } else {
+                setIsPlaying(true);
+              }
+            }}
+          >
+            {!isPlaying ? "Play" : "Stop"}
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
